fd69b8862f0b04facdee0002f4224273d87e8569
Tuomas Salmi <salmi.tuomas@gmail.com>
2014-07-21 16:26:34 +0300

    Get-serializer serializes now the arrays like language, tags and urls.

--- sites/basaar/apps/api/serializers.py
+++ sites/basaar/apps/api/serializers.py
@@ -5,6 +5,8 @@ from oscar.core.loading import get_class, get_model
 
 Product = get_model('catalogue', 'Product')
 EmbeddedMedia = get_model('catalogue', 'EmbeddedMedia')
+Tags = get_model('catalogue', 'Tags')
+Language = get_model('catalogue', 'Language')
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -31,18 +33,26 @@ class MaterialItemSerializer(serializers.HyperlinkedModelSerializer):
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
 
-    mediaurlLookup = serializers.SerializerMethodField('mediaUrlLookup')
+    embedMedia = serializers.SerializerMethodField('mediaUrlLookup')
+    tags = serializers.SerializerMethodField('tagsLookup')
+    languages = serializers.SerializerMethodField('languageLookup')
 
     def mediaUrlLookup(self, obj):
         objs = EmbeddedMedia.objects.filter(product=obj)
         #serializer = MediaUrlSerializer(objs, many=True)
         return objs #serializer.data
 
+    def tagsLookup(self, obj):
+        return Tags.objects.filter(hasTags=obj)
+
+    def languageLookup(self, obj):
+        return Language.objects.filter(hasLanguage=obj)
+
     class Meta:
         model = Product
         fields = ('uuid', 'title', 'description', 'materialUrl', 'moreInfoUrl', 'version',
         'contributionDate', 'maxAge', 'minAge', 'contentLicense',
-        'dataLicense', 'copyrightNotice', 'attributionText', 'attributionURL', 'mediaurlLookup')
+        'dataLicense', 'copyrightNotice', 'attributionText', 'attributionURL', 'embedMedia', 'tags', 'languages')
         #read_only_fields = ('mTitle', 'slug')
 
 class APINodeSerializer(serializers.HyperlinkedModelSerializer):
@@ -56,3 +66,13 @@ class MediaUrlSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = EmbeddedMedia
         fields = ('url')
+
+class TagsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Tags
+        fields = ('name')
+
+class LanguageSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Language
+        fields = ('name')
