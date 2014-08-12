2509345703b82ed32de649e28486cec014d1a055
Joonas Reinikka <joonas.reinikka@gmail.com>
2014-08-12 11:12:44 +0300

    mepinId field

--- sites/basaar/apps/user/models.py
+++ sites/basaar/apps/user/models.py
@@ -5,4 +5,5 @@ from oscar.core import compat
 from oscar.apps.customer.abstract_models import AbstractUser
 
 class User(AbstractUser):
-    oid = models.CharField(max_length=255)
+    oid = models.CharField(max_length=255, blank=True)
+    mepinId = models.CharField(max_length=255, blank=True)
