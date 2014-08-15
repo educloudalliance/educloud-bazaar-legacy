c1f5eadb44ec9adeb44378d393ec7b0692fd48b2
Joonas Reinikka <joonas.reinikka@gmail.com>
2014-08-15 15:16:02 +0300

    Username field Locales Few templates changes

--- sites/basaar/apps/user/models.py
+++ sites/basaar/apps/user/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.core import validators
+from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 
 from oscar.core import compat
 from oscar.apps.customer.abstract_models import AbstractUser
 
 class User(AbstractUser):
+    username = models.CharField(_('username'), max_length=30, unique=True,
+        help_text=_('Required. 30 characters or fewer. Letters, digits and '
+                    '@/./+/-/_ only.'),
+        validators=[
+            validators.RegexValidator(r'^[\w.@+-]+$',
+                                      _('Enter a valid username. '
+                                        'This value may contain only letters, numbers '
+                                        'and @/./+/-/_ characters.'), 'invalid'),
+        ])
     oid = models.CharField(max_length=255, blank=True)
     mepinId = models.CharField(max_length=255, blank=True)
