c3ab88bdd6d82939fd85b9b0324575a6b912e4a6
Joonas Reinikka <joonas.reinikka@gmail.com>
2014-08-21 15:47:26 +0300

    Added productFormat-model

--- sites/basaar/apps/catalogue/admin.py
+++ sites/basaar/apps/catalogue/admin.py
@@ -2,15 +2,20 @@ from django.contrib import admin
 from oscar.core.loading import get_model
 from treebeard.admin import TreeAdmin
 
+class productFormatAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug" : ("name",)}
+
 Tags = get_model('catalogue', 'Tags')
 Language = get_model('catalogue', 'Language')
 EmbeddedMedia = get_model('catalogue', 'EmbeddedMedia')
 Product = get_model('catalogue', 'Product')
 Category = get_model('catalogue', 'Category')
 ProductClass = get_model('catalogue', 'ProductClass')
+ProductFormat = get_model('catalogue', 'ProductFormat')
 
 admin.site.register(Language)
 admin.site.register(EmbeddedMedia)
+admin.site.register(ProductFormat, productFormatAdmin)
 admin.site.register(Tags)
 admin.site.register(Product)
 admin.site.register(Category)
