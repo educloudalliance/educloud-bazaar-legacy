3c59852236fde1ce67b083f46d8e9071dc92e100
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2014-10-07 09:11:25 +0300

    Shibboleth features are integrated in customer app. Removed unused shibboleth app. Renamed User to more descriptive BazaarUser.

--- bazaar/apps/user/admin.py
+++ bazaar/apps/user/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from models import BazaarUser as User
+from models import BazaarUser
       
 # Register your models here.
-admin.site.register(User)
+admin.site.register(BazaarUser)
