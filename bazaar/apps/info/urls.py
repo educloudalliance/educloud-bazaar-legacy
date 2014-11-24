396cdd19672e45becf75373125d92cc455905e88
Joonas Reinikka <joonas.reinikka@gmail.com>
2014-11-24 03:00:22 +0200

    - Changed info/rekisteriseloste into info/disclaimer - Added finnish disclaimer text

--- bazaar/apps/info/urls.py
+++ bazaar/apps/info/urls.py
@@ -5,5 +5,5 @@ from apps.info import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^rekisteriseloste/$', views.rekisteri, name='rekisteri'),
+    url(r'^disclaimer/$', views.disclaimer, name='disclaimer'),
 )
